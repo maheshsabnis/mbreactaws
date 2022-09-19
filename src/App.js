4d4255de94d72fb770b30527a197aa4bcb43165c
maheshsabnis <sabnis_m@hotmail.com>
2022-09-19 15:08:02 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 
+import React,{useState} from 'react';
+
+import  axios   from "axios";
+ 
 import './App.css';
 
+
 function App() {
+
+  const [data,setData] = useState([]);
+  const getData=()=>{
+    axios.get("http://a7ca5ed9e114b45e096f748989625ddb-1177704058.us-east-2.elb.amazonaws.com:8080/WeatherForecast")
+      .then((resp)=>{
+         setData(resp.data);
+      }).catch((error)=>{
+        setData(error);
+      });  
+  };
+
   return (
     <div className="App">
          <h1>The React app on AWS deployed using AWS Amplify</h1>
+         <button onClick={getData}>Get Data</button>
+         <br/>
+         {JSON.stringify(data)}
     </div>
   );
 }
